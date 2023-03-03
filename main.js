d38b9506d3b06b7eb76f60ef66c4c487be8f8955
Marcel Drąg <marcel.drag@interia.pl>
2023-03-03 21:58:42 +0100

    Added semicolons to renderer.js and main.js

--- main.js
+++ main.js
@@ -1,62 +1,61 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron')
-const path = require('path')
-const fs = require('fs')
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const path = require('path');
+const fs = require('fs');
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
         }
     })
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html');
 }
 
 async function getRootDirs() {
-    const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory']})
+    const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory']});
     if (canceled) {
-        return
+        return;
     } else {
-        return filePaths[0]
+        return filePaths[0];
     }
 }
 
 function getLocContent(event, newLocPath, justFiles = false) {
-    contentMixed = fs.readdirSync(newLocPath)
-    dirContent = []
-    fileContent = []
-    contentSeparated = []
+    contentMixed = fs.readdirSync(newLocPath);
+    dirContent = [];
+    fileContent = [];
+    contentSeparated = [];
 
     if (justFiles) {
         for (let contentElement of contentMixed) {
             try {
                 if (fs.statSync(newLocPath+"\\"+contentElement).isFile()) {
-                    fileContent.push(contentElement)
+                    fileContent.push(contentElement);
                 }
             } catch (err) {}
         }
 
-        contentSeparated = {dirContent, fileContent}
-        return contentSeparated  
+        contentSeparated = {dirContent, fileContent};
+        return contentSeparated;
         
     } else {
         for (let contentElement of contentMixed) {
             try {
                 if (fs.statSync(newLocPath+"\\"+contentElement).isDirectory()) {
-                    dirContent.push(contentElement)
+                    dirContent.push(contentElement);
                 } else if (fs.statSync(newLocPath+"\\"+contentElement).isFile()) {
-                    fileContent.push(contentElement)
+                    fileContent.push(contentElement);
                 }
             } catch (err) {}
         }
 
-        contentSeparated = {dirContent, fileContent}
-        return contentSeparated   
-
+        contentSeparated = {dirContent, fileContent};
+        return contentSeparated;
     }
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle('chanel1', getRootDirs)
-    ipcMain.handle('chanel2', getLocContent)
-    createWindow()
+    ipcMain.handle('chanel1', getRootDirs);
+    ipcMain.handle('chanel2', getLocContent);
+    createWindow();
 })
--- renderer.js
+++ renderer.js
@@ -1,87 +1,87 @@
 const dirsDiv = document.getElementById('dirsDiv');
 const filesDiv = document.getElementById('filesDiv');
-const backButton = document.getElementById('backButton')
-const addDirButton = document.getElementById('addDirButton')
-const forwardButton = document.getElementById('forwardButton')
+const backButton = document.getElementById('backButton');
+const addDirButton = document.getElementById('addDirButton');
+const forwardButton = document.getElementById('forwardButton');
 
-let locHistoryIndex = 0
-let locHistory = []
-let addedRootDirs = {dirContent: []}
-let scrollHistory = []
-let selectedDirs = []
+let locHistoryIndex = 0;
+let locHistory = [];
+let addedRootDirs = {dirContent: []};
+let scrollHistory = [];
+let selectedDirs = [];
 
 function removeDisplayedContent(targetsToBeDeleted = 'everything') {
     if (targetsToBeDeleted == 'everything') {
-        const dirsDivChildren = Array.from(dirsDiv.children)
-        const filesDivChildren = Array.from(filesDiv.children)
+        const dirsDivChildren = Array.from(dirsDiv.children);
+        const filesDivChildren = Array.from(filesDiv.children);
     
         for (let child of dirsDivChildren) {
-            child.remove()
+            child.remove();
         }
 
         for (let child of filesDivChildren) {
-            child.remove()
+            child.remove();
         }
     } else {
-        const selectorEncoded = encodeURIComponent(targetsToBeDeleted)
-        const fileDivsToBeDeleted = document.getElementsByClassName(selectorEncoded)
-        const fileDivsToBeDeletedArray = Array.from(fileDivsToBeDeleted)
-        console.log(fileDivsToBeDeletedArray)
+        const selectorEncoded = encodeURIComponent(targetsToBeDeleted);
+        const fileDivsToBeDeleted = document.getElementsByClassName(selectorEncoded);
+        const fileDivsToBeDeletedArray = Array.from(fileDivsToBeDeleted);
+        console.log(fileDivsToBeDeletedArray);
 
         for (let fileDiv of fileDivsToBeDeletedArray) {
-            fileDiv.remove()
+            fileDiv.remove();
         }
     }
 }
 
 function encapsulatedCreateLocObject(objName, cssSelector, targetAttribute, targetDiv) {
-    const div = document.createElement('div')
-    div.innerText = objName
-    const cssSelectorEncoded = encodeURIComponent(cssSelector)
-    div.setAttribute(targetAttribute, cssSelectorEncoded)
-    div.classList.add("locContentObject")
-    targetDiv.append(div)
+    const div = document.createElement('div');
+    div.innerText = objName;
+    const cssSelectorEncoded = encodeURIComponent(cssSelector);
+    div.setAttribute(targetAttribute, cssSelectorEncoded);
+    div.classList.add("locContentObject");
+    targetDiv.append(div);
 }
 
 function createLocObject(targetDiv, objName, selectorString = null) {
     if (locHistoryIndex > 0) {
         if (targetDiv == dirsDiv) {
-            const cssSelector = selectorString + '\\' + objName
-            encapsulatedCreateLocObject(objName, cssSelector, "id", targetDiv)
+            const cssSelector = selectorString + '\\' + objName;
+            encapsulatedCreateLocObject(objName, cssSelector, "id", targetDiv);
         } else if (targetDiv == filesDiv) {
-            const cssSelector = selectorString
-            encapsulatedCreateLocObject(objName, cssSelector, "class", targetDiv)
+            const cssSelector = selectorString;
+            encapsulatedCreateLocObject(objName, cssSelector, "class", targetDiv);
         }
     } else {
         if (targetDiv == dirsDiv) {
-            const cssSelector = objName
-            encapsulatedCreateLocObject(objName, cssSelector, "id", targetDiv)
+            const cssSelector = objName;
+            encapsulatedCreateLocObject(objName, cssSelector, "id", targetDiv);
         } else if (targetDiv == filesDiv) {
-            const cssSelector = selectorString
-            encapsulatedCreateLocObject(objName, cssSelector, "class", targetDiv)
+            const cssSelector = selectorString;
+            encapsulatedCreateLocObject(objName, cssSelector, "class", targetDiv);
         }
     }
 }
 
 function createAllLocObjects(targetDiv, objNamesArray, selectorString = null) {
     for (let objName of objNamesArray) {
-        createLocObject(targetDiv, objName, selectorString)
+        createLocObject(targetDiv, objName, selectorString);
     }
 }
 
 function buildLocPath(useOriginalLocHistoryOrCopy = 'fromOriginal', newElement = null) {
     if (useOriginalLocHistoryOrCopy == 'fromCopy') {
-        let locHistoryCopy = [...locHistory]
-        locHistoryCopy.splice(locHistoryIndex, locHistoryCopy.length - locHistoryIndex)
-        const newLocPath = locHistoryCopy.join('\\')
+        let locHistoryCopy = [...locHistory];
+        locHistoryCopy.splice(locHistoryIndex, locHistoryCopy.length - locHistoryIndex);
+        const newLocPath = locHistoryCopy.join('\\');
     
-        return newLocPath    
+        return newLocPath;
     } else if (useOriginalLocHistoryOrCopy == 'fromOriginal') {
-        locHistory.splice(locHistoryIndex, locHistory.length - locHistoryIndex)
-        locHistory.push(newElement)
-        const newLocPath = locHistory.join('\\')
+        locHistory.splice(locHistoryIndex, locHistory.length - locHistoryIndex);
+        locHistory.push(newElement);
+        const newLocPath = locHistory.join('\\');
 
-        return newLocPath
+        return newLocPath;
     }
 }
 
@@ -89,153 +89,148 @@ function navigationWithButtons() {
     if (locHistoryIndex > 0) {
         if (locHistoryIndex == locHistory.length) {
             
-            addDirButton.disabled = true
-            backButton.disabled = false
-            forwardButton.disabled = true
+            addDirButton.disabled = true;
+            backButton.disabled = false;
+            forwardButton.disabled = true;
 
-            const newLocPath = locHistory.join('\\')
+            const newLocPath = locHistory.join('\\');
 
-            return newLocPath
+            return newLocPath;
         } else {
             
-            addDirButton.disabled = true
-            backButton.disabled = false
-            forwardButton.disabled = false
+            addDirButton.disabled = true;
+            backButton.disabled = false;
+            forwardButton.disabled = false;
 
-            const newLocPath = buildLocPath('fromCopy')
+            const newLocPath = buildLocPath('fromCopy');
 
-            return newLocPath
+            return newLocPath;
         }
     } else {
 
-            addDirButton.disabled = false
-            backButton.disabled = true
-            forwardButton.disabled = false
+            addDirButton.disabled = false;
+            backButton.disabled = true;
+            forwardButton.disabled = false;
 
-            return addedRootDirs
+            return addedRootDirs;
     }
 }
 
 function rememberScrollHeight() {
-    scrollHistory.splice(locHistoryIndex, scrollHistory.length - locHistoryIndex)
-    scrollHistory.push(dirsDiv.scrollTop)
+    scrollHistory.splice(locHistoryIndex, scrollHistory.length - locHistoryIndex);
+    scrollHistory.push(dirsDiv.scrollTop);
 }
 
 function remindScrollHeight() {
-    dirsDiv.scrollTop = scrollHistory[locHistoryIndex]
+    dirsDiv.scrollTop = scrollHistory[locHistoryIndex];
 }
 
 addDirButton.addEventListener('click', async (event) => {
-    event.stopPropagation()
-    const rootDir = await window.electronAPI.callWithIpcGetRootDirs()
+    event.stopPropagation();
+    const rootDir = await window.electronAPI.callWithIpcGetRootDirs();
     if (rootDir != null && !addedRootDirs.dirContent.includes(rootDir)) {
-        addedRootDirs.dirContent.push(rootDir)
-        createLocObject(dirsDiv, rootDir, rootDir)
+        addedRootDirs.dirContent.push(rootDir);
+        createLocObject(dirsDiv, rootDir, rootDir);
     }
 })
 
 dirsDiv.addEventListener('dblclick', async (event) => {
-    rememberScrollHeight()
-    event.stopPropagation()
+    rememberScrollHeight();
+    event.stopPropagation();
     
-    const target = event.target
+    const target = event.target;
     if (target !== event.currentTarget && !event.ctrlKey) {
-        removeDisplayedContent()
-        selectedDirs = []
+        removeDisplayedContent();
+        selectedDirs = [];
 
         if (locHistoryIndex == 0) {
-            addDirButton.disabled = true
-            forwardButton.disabled = true
-            backButton.disabled = false
+            addDirButton.disabled = true;
+            forwardButton.disabled = true;
+            backButton.disabled = false;
 
-            locHistory = []
-            locHistory.push(target.innerText)
-            const newLocPath = target.innerText
-            locHistoryIndex += 1
+            locHistory = [];
+            locHistory.push(target.innerText);
+            const newLocPath = target.innerText;
+            locHistoryIndex += 1;
 
 
-            const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath)
-            createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath)
-            createAllLocObjects(filesDiv, locContent.fileContent, newLocPath)
+            const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath);
+            createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath);
+            createAllLocObjects(filesDiv, locContent.fileContent, newLocPath);
 
         } else if (locHistoryIndex > 0) {
-            forwardButton.disabled = true
-            backButton.disabled = false
+            forwardButton.disabled = true;
+            backButton.disabled = false;
     
-            const newLocPath = buildLocPath('fromOriginal', target.innerText)
-            locHistoryIndex += 1
+            const newLocPath = buildLocPath('fromOriginal', target.innerText);
+            locHistoryIndex += 1;
     
-            const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath)
+            const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath);
     
-            createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath)
-            createAllLocObjects(filesDiv, locContent.fileContent, newLocPath)
+            createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath);
+            createAllLocObjects(filesDiv, locContent.fileContent, newLocPath);
         }
     }
 })
 
 backButton.addEventListener('click', async (event) => {
-    event.stopPropagation()
-    removeDisplayedContent()
+    event.stopPropagation();
+    removeDisplayedContent();
 
-    selectedDirs = []
+    selectedDirs = [];
 
-    locHistoryIndex -= 1
+    locHistoryIndex -= 1;
 
     if (locHistoryIndex > 0) {
-        const newLocPath = navigationWithButtons()
-        const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath)
-    
-        createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath)
-        createAllLocObjects(filesDiv, locContent.fileContent, newLocPath)    
+        const newLocPath = navigationWithButtons();
+        const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath);
+        createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath);
+        createAllLocObjects(filesDiv, locContent.fileContent, newLocPath);
     } else {
-        const locContent = navigationWithButtons()
-        createAllLocObjects(dirsDiv, locContent.dirContent)
+        const locContent = navigationWithButtons();
+        createAllLocObjects(dirsDiv, locContent.dirContent);
     }
-    remindScrollHeight()
+    remindScrollHeight();
 })
 
 forwardButton.addEventListener('click', async (event) => {
-    event.stopPropagation()
-    removeDisplayedContent()
+    event.stopPropagation();
+    removeDisplayedContent();
 
-    selectedDirs = []
+    selectedDirs = [];
+    locHistoryIndex += 1;
 
-    locHistoryIndex += 1
-
-    const newLocPath = navigationWithButtons()
-    const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath)
+    const newLocPath = navigationWithButtons();
+    const locContent = await window.electronAPI.callWithIpcGetLocContent(newLocPath);
     
-    createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath)
-    createAllLocObjects(filesDiv, locContent.fileContent, newLocPath)  
+    createAllLocObjects(dirsDiv, locContent.dirContent, newLocPath);
+    createAllLocObjects(filesDiv, locContent.fileContent, newLocPath);
     
-    remindScrollHeight()
+    remindScrollHeight();
 })
 
 dirsDiv.addEventListener('click', async (event) => {
-    event.stopPropagation()
-    const target = event.target
-    let selectedLocPath
+    event.stopPropagation();
+    const target = event.target;
+    let selectedLocPath;
 
     if (event.ctrlKey && target !== event.currentTarget) {
         
         if (locHistoryIndex == 0) {
-            selectedLocPath = target.innerText
+            selectedLocPath = target.innerText;
         } else {
-            selectedLocPath = buildLocPath('fromCopy')
-            selectedLocPath += '\\' + target.innerText
+            selectedLocPath = buildLocPath('fromCopy');
+            selectedLocPath += '\\' + target.innerText;
         }   
 
         if (!selectedDirs.includes(selectedLocPath)) {
-            const locContent = await window.electronAPI.callWithIpcGetLocContent(selectedLocPath)
-
-            createAllLocObjects(filesDiv, locContent.fileContent, selectedLocPath)
-            selectedDirs.push(selectedLocPath)
+            const locContent = await window.electronAPI.callWithIpcGetLocContent(selectedLocPath);
+            createAllLocObjects(filesDiv, locContent.fileContent, selectedLocPath);
+            selectedDirs.push(selectedLocPath);
         } else {
-            removeDisplayedContent(selectedLocPath)
-            
-            const selectedLocPathIndex = selectedDirs.indexOf(selectedLocPath)
-            selectedDirs.splice(selectedLocPathIndex, 1)
+            removeDisplayedContent(selectedLocPath);         
+            const selectedLocPathIndex = selectedDirs.indexOf(selectedLocPath);
+            selectedDirs.splice(selectedLocPathIndex, 1);
         }
     }
-    console.log("filesDiv children:",filesDiv.children)
 })
